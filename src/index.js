d1003c5d3118f73f92450473b2433813503c2e85
kenza17-urc <khammoudi17@gmail.com>
2024-06-20 13:20:28 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 
-const initialData = window.__INITIAL_DATA__;
 
 hydrateRoot(
   document.getElementById('root'),
   <React.StrictMode>
-    <App initialTodos={initialData} />
+    <App initialTodos />
   </React.StrictMode>
 );
 
